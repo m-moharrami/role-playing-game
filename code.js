c00937627414db2f2da4c63d57bb66c4d23a15e1
m-moharrami <moharram.m98@gmail.com>
2022-05-27 15:35:20 +0430

    made renderCharacter() function in order to not repeat the code

--- code.js
+++ code.js
@@ -1,17 +1,24 @@
-document.getElementById('hero').innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Wizard </h4>
-        <img class="avatar" src="images/wizard.png"/>
-        <p class="health">health: <b> 60 </b></p>
-        <div class="dice-container"><div class="dice"> 6 </div></div>
-    </div>  
-`;
+const heroElementId = "hero";
+const heroName = "Wizard";
+const heroAvatar = "images/wizard.png";
+const heroHealth = "60";
+const heroDiceRoll = 6;
 
-document.getElementById('monster').innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Orc </h4>
-        <img class="avatar" src="images/orc.png"/>
-        <p class="health">health: <b> 10 </b></p>
-        <div class="dice-container"><div class="dice"> 4 </div></div>
-    </div>
-`;
+const monsterElementId = "monster";
+const monsterName = "orc";
+const monsterAvatar = "images/orc.png";
+const monsterHealth = "10";
+const monsterDiceRoll = 4;
+
+function renderCharacter(elementId, name, avatar, health, diceRoll) {
+    document.getElementById(elementId).innerHTML = 
+    `<div class="character-card">
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}"/>
+        <p class="health">health: <b> ${health} </b></p>
+        <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
+    </div>`;
+}
+
+renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll);
+renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll);
