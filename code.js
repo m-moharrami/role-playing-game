b605452bcf0b88dda6a2aa62d0f80b3096708faa
m-moharrami <moharram.m98@gmail.com>
2022-06-01 23:12:12 +0430

    generate random numbers for dice

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
+function getDiceRollArray(diceCount){
+    const newDiceRolls = [];
+    for(let i = 0; i < diceCount; i++){
+        newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return newDiceRolls;
+}
+
 const hero = {
     elementId: "hero",
     name: "Wizard",
