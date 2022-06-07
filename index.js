a896d548d9b626e1a1a2c43a58da1e6d8003de11
m-moharrami <moharram.m98@gmail.com>
2022-06-07 21:15:31 +0430

    initialize attack function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import characterData from './data.js';
 import Character from './character.js';
 
-function render(){
-    document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
-    document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+function attack(){
+    render();
 }
+
+function render(){
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml();
+}
+
+document.getElementById("attack-button").addEventListener('click', attack)
 
 const wizard = new Character(characterData.hero);
 const orc = new Character(characterData.monster);
