98d83dfef23fb972832b9436fdf5b8383aa0c201
m-moharrami <moharram.m98@gmail.com>
2022-06-13 00:05:19 +0430

    display health bar for each character

--- Character.js
+++ Character.js
@@ -25,13 +25,24 @@ function Character(data){
         }
     }
 
+    this.getHealthBarHtml = function(){
+        const percent = getPercentage(this.health, this.maxHealth);
+        console.log(percent); //Temporary, for test
+        return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}"
+                        style="width: ${percent}%;">
+                    </div>
+                </div>`
+    }
+
     this.getCharacterHtml = function(){
         const { elementId, name, avatar, health, diceCount, diceArray } = this;
-        let diceHtml = this.getDiceHtml(diceCount)
+        const healthBar = this.getHealthBarHtml();
         return `<div class="character-card">
                     <h4 class="name">${name}</h4>
                     <img class="avatar" src="${avatar}" />
                     <div class="health">health:<b>${health}</b></div>
+                    ${healthBar}
                     <div class="dice-container">${diceArray}</div>
                 </div>`;
     }
